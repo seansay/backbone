91216b32cbe16638baba2347b037d133373627d4
Yongning Liang <liangyongning@yahoo.com>
2012-06-03 23:58:08 +0800

    backbone-localstorage.js 翻译

--- examples/backbone-localstorage.js
+++ examples/backbone-localstorage.js
@@ -1,19 +1,18 @@
-// A simple module to replace `Backbone.sync` with *localStorage*-based
-// persistence. Models are given GUIDS, and saved into a JSON object. Simple
-// as that.
+// 一个替代 `Backbone.sync` 的简单模块, 使用HTML5的 *localStorage* 来做持久化.
+// 模型都有其自己唯一的GUID, 以JSON对象的形式保存. 就这么简单.
 
-// Generate four random hex digits.
+// 生成四位随机的十六进制数
 function S4() {
    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
 };
 
-// Generate a pseudo-GUID by concatenating random hexadecimal.
+// 生成一个伪GUID, 连接多个随机十六进制数.
 function guid() {
    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
 };
 
-// Our Store is represented by a single JS object in *localStorage*. Create it
-// with a meaningful name, like the name you'd give a table.
+// Store代表了一个保存在 *localStorage* 的JSON对象. 以有意义的名称来命名, 
+// 就像给数据库里的一个表命名一样.
 var Store = function(name) {
   this.name = name;
   var store = localStorage.getItem(this.name);
@@ -22,13 +21,13 @@ var Store = function(name) {
 
 _.extend(Store.prototype, {
 
-  // Save the current state of the **Store** to *localStorage*.
+  // 保存 **Store** 的当前状态到 *localStorage* 里.
   save: function() {
     localStorage.setItem(this.name, JSON.stringify(this.data));
   },
 
-  // Add a model, giving it a (hopefully)-unique GUID, if it doesn't already
-  // have an id of it's own.
+  // 增加一个模型, 如果它自己还没有id的话.
+  // 给它生成一个唯一的(希望是唯一的)GUID,
   create: function(model) {
     if (!model.id) model.id = model.attributes.id = guid();
     this.data[model.id] = model;
@@ -36,24 +35,24 @@ _.extend(Store.prototype, {
     return model;
   },
 
-  // Update a model by replacing its copy in `this.data`.
+  // 更新一个模型, 以 `this.data` 来更新.
   update: function(model) {
     this.data[model.id] = model;
     this.save();
     return model;
   },
 
-  // Retrieve a model from `this.data` by id.
+  // 根据id从 `this.data` 里获取一个模型.
   find: function(model) {
     return this.data[model.id];
   },
 
-  // Return the array of all models currently in storage.
+  // 返回所有模型的数组.
   findAll: function() {
     return _.values(this.data);
   },
 
-  // Delete a model from `this.data`, returning it.
+  // 从 `this.data` 里删除一个模型, 并返回这个模型.
   destroy: function(model) {
     delete this.data[model.id];
     this.save();
@@ -62,8 +61,8 @@ _.extend(Store.prototype, {
 
 });
 
-// Override `Backbone.sync` to use delegate to the model or collection's
-// *localStorage* property, which should be an instance of `Store`.
+// 重写 `Backbone.sync` , 委托到模型或集合的
+// *localStorage* 属性, 它应该是 `Store` 的一个实例.
 Backbone.sync = function(method, model, options) {
 
   var resp;
